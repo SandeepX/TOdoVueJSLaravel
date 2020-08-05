9400784e2d04ba6745e7ce920cec8b03ad964bdc
sandeeppant <sandeeppant024@gmail.com>
2020-08-05 19:00:07 +0545

    vue laravel single page appilication

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,5 @@ Route::get('tasks', 'TaskController@index');
 Route::get('tasks/{id}', 'TaskController@show');
 Route::post('tasks', 'TaskController@store');
 Route::put('tasks/{id}', 'TaskController@update');
+
 Route::delete('tasks/{id}', 'TaskController@delete');
